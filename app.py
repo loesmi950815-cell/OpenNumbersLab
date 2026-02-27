98fd88b027a330e713ecb852c42bf72c66fcb498
loesmi950815-cell <loesmi950815@gmail.com>
2026-02-27 16:49:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,8 @@ def work_hours():
         result = hours_per_day * days
     return render_template("workhours.html", result=result)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
